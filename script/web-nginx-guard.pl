86d662df78071944bab1b55d72b761375f98296f
slegga3 <sslegga@gmail.com>
2020-02-11 09:00:49 +0100

    overllogging

--- script/web-nginx-guard.pl
+++ script/web-nginx-guard.pl
@@ -48,7 +48,7 @@ get '/' => sub {
 			my $claims;
 			eval {
 				my $claims = Mojo::JWT->new(secret => $c->app->secrets->[0])->decode($jwt);
-			} or $c->app->log->error('Did not manage to validate jwt "$jwt" '.$!.' '.$@);
+			} or $c->app->log->error('Did not manage to validate jwt "'.$jwt.'" '.$!.' '.$@. "secret: ". $c->app->secrets->[0]);
 			if ($claims) {
 				$c->app->log->info('claims is '.j($claims));
 				$user = $claims->{user};
