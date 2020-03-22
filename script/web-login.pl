f217822e8bbc8a35a77579dc7d4830603b0abcf9
Stein Hammer <steihamm@gmail.com>
2020-03-22 14:52:55 +0100

    Virkende path manipulering

--- script/web-login.pl
+++ script/web-login.pl
@@ -20,9 +20,9 @@ use lib $lib;
 use SH::UseLib;
 use Mojolicious::Commands;
 #use Mojolicious::Lite;
-#use Model::GetCommonConfig;
-#my $cfg = Model::GetCommonConfig->new->get_mojoapp_config($0);
-#warn $cfg->{hypnotoad}->{service_path};
+use Model::GetCommonConfig;
+my $cfg = Model::GetCommonConfig->new->get_mojoapp_config($0);
+warn $cfg->{hypnotoad}->{service_path};
 #my $rs=app->routes;
 #$rs->route($cfg->{hypnotoad}->{service_path})->detour('Login',{secrets=>$cfg->{secrets} });
 #app->routes($rs);
@@ -37,5 +37,8 @@ web-login.pl - Master login. The main webserver script.
 =cut
 
 # Start command line interface for application
- Mojolicious::Commands->start_app('Login');
+my $app = Mojo::Server->new->build_app('Login');
+$app->routes->pattern(Mojolicious::Routes::Pattern->new('/'.$cfg->{hypnotoad}->{service_path}));
+$app->start;
+#Mojolicious::Commands->start_app('Login');
 
