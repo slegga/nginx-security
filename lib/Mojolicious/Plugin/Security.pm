6c48dd4b9ce8959a44f2edbc22e0353f4bf4e61c
Stein Hammer <steihamm@gmail.com>
2020-04-02 19:45:52 +0200

    Legger til å kunne angi attributter ved definisjon

--- lib/Mojolicious/Plugin/Security.pm
+++ lib/Mojolicious/Plugin/Security.pm
@@ -168,28 +168,14 @@ Auto called from Mojolicious. Do the setup.
 =cut
 
 sub register {
-  	my ( $self, $app ) = @_;
-
-  	# Register hook
-#  	if ( my $path = $app->config->{hypnotoad}->{service_path} ) {
- # 		my @path_parts = grep /\S/, split m{/}, $path;
-#		$app->hook(before_dispatch =>  sub {
-#			my ( $c ) = @_;
-#			my $url = $c->req->url;
-#			my $base = $url->base;
-#			$base->path ( @path_parts);
-#			if (index($path, $url->path) == 0) {
-#				$url->path(substr($url->path,length $path)); # remove base
-#			}
-#			$base->path->trailing_slash(1);
-#			$url->path->leading_slash(0);
-#		});
-#	}
+  	my ( $self, $app, $attributes ) = @_;
 
 	# Register helpers
 	for my $h(qw/user unauthenticated url_logout/ ) {
     	$app->helper($h => sub {$self->$h(@_)});
 	}
-
+    for my $key (keys %$attributes) {
+        $self->$key($attributes->{$key});
+    }
 }
 1;
