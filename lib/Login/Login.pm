05abbac1173efa5a5d9fb84d37add61e4e4d8cd7
Stein Hammer <steihamm@gmail.com>
2020-04-17 14:59:39 +0200

    feil med app->log nytt forsøk

--- lib/Login/Login.pm
+++ lib/Login/Login.pm
@@ -113,18 +113,18 @@ sub oauth2_google {
     $c->oauth2->get_token_p(google => $get_token_args)->then(sub {
         return unless my $provider_res = shift; # Redirct to Facebook
 #        $c->session(token => $provider_res->{openid});
-		$c->app->log(warn "id_token=".$provider_res->{id_token});
+		$c->app->log->warn( "id_token=".$provider_res->{id_token});
 
         my $tmp = (split(/\./, $provider_res->{id_token}))[1];
-   		$c->app->log(warn "id_tokenno2=".$tmp);
+   		$c->app->log->warn( "id_tokenno2=".$tmp);
 
 #no code here
         my $tmp2 = decode_base64($tmp);
-   		$c->app->log(warn "id_tokenno2decoded=".$tmp2);
+   		$c->app->log->warn( "id_tokenno2decoded=".$tmp2);
 		my $payload = from_json($tmp2);
         my $user;
         $user = $payload->{email} if ref $payload;
-		$c->app->log(warn "payload=".dumper($payload));
+		$c->app->log->warn( "payload=".dumper($payload));
 #        delete $tmp->{id_token}; #tar for mye plass i cookie inneholder base64 {"alg":"RS256","kid":"6fcf413224765156b48768a42fac06496a30ff5a","typ":"JWT"}
         $c->session(google_idt => $payload );
         $c->session(user => $user);
